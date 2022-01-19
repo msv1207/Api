e70b0d08b7b0ff3c236a8bca7b45508043af8747
Valera <valerakaktotak@gmail.com>
2022-01-19 19:13:32 +0200

    create migration

---
+++ database/migrations/2022_01_19_164905_films.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Films extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('films', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('original_title');
+            $table->longtext('description');
+            $table->date('release_date');
+            $table->text('poster_path');
+            $table->string('language');
+            $table->float('popularity');
+            $table->float('vote_average');
+            $table->boolean('adult');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('films');
+
+    }
+}
---
+++ database/migrations/2022_01_19_165743_film_category.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class FilmCategory extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('film_category', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('film_id')->unsigned();
+            $table->foreign('film_id')->references('id')->on('films');
+            $table->integer('category_id')->unsigned();
+            $table->foreign('category_id')->references('id')->on('category');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('film_category');
+    }
+}
