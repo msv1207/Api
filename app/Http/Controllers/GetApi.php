91099355d36785de78da0011528e6a7c5f7e3175
Misha Vlasov <Vlasovm452@gmail.com>
2022-01-20 02:00:16 +0200

    save in db from api

--- app/Http/Controllers/GetApi.php
+++ app/Http/Controllers/GetApi.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ApiModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
+use App\Models;
 
 
 class GetApi extends Controller
@@ -11,7 +14,24 @@ class GetApi extends Controller
     public function GetApi()
     {
         $response =
-            Http::get('https://api.themoviedb.org/3/movie/550?api_key=5cf7a7c1c45476c43ef0d43846756912');
-           return json_decode($response);
+            Http::get('https://api.themoviedb.org/3/movie/popular?api_key=5cf7a7c1c45476c43ef0d43846756912');
+        $response=(json_decode($response));
+
+        $response=($response->results);
+        foreach ($response as $value ) {
+
+            DB::table("films")->insert([
+                'adult' => TRUE,
+                'title' => "$value->original_title",
+                'original_title' => "$value->original_title",
+                'description' => "$value->overview",
+                'release_date' => "$value->release_date",
+                'poster_path' => "$value->poster_path",
+                'language' => "$value->original_language",
+                'popularity' => "$value->popularity",
+                'vote_average' => "$value->vote_average",
+            ]);
+        }
     }
+
 }
