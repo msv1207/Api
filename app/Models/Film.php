ac802c713ef8c253f2f631120a86da187f038390
ArtemKhabarov21 <artem.khabarov@outlook.com>
2022-01-21 18:36:54 +0200

    add models Category and Film

---
+++ app/Models/Film.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Film extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'original_id',
+        'adult',
+        'title',
+        'original_title',
+        'description',
+        'release_date',
+        'poster_path',
+        'language',
+        'popularity',
+        'vote_average',
+        'budget'
+    ];
+}
