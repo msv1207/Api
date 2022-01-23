951f67fc4d8796b10f6efaca3e43326b6d4dd6b6
Misha Vlasov <Vlasovm452@gmail.com>
2022-01-23 02:25:27 +0200

    testing

---
+++ app/Models/Category.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'original_id',
+        'title'
+    ];
+
+    public function films(){
+
+       return $this->belongsToMany(Film::class)->withPivot('column1', 'column2');
+//        return $this->belongsToMany(Film::class, 'film_category', 'original_id', 'original_id');
+
+    }
+}
---
+++ app/Models/Film.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Film extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'original_id',
+        'adult',
+        'title',
+        'original_title',
+        'description',
+        'release_date',
+        'poster_path',
+        'language',
+        'popularity',
+        'vote_average',
+        'budget'
+    ];
+//    protected $table = 'films';
+
+    public function categories(){
+        return  $this->belongsToMany(Category::class, 'film_category', 'category_id');
+    }
+}
